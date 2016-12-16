927662d5e42a3ebccc99a257d333e0c33b01ecd6
Justin Mulkin <justinmulkin@comcast.net>
2016-12-16 16:20:03 -0500

    Attempting to get the state machine to function correctly

--- GameAI/pathfinding/Pathfinding/Game/EnemyStateMachine.h
+++ GameAI/pathfinding/Pathfinding/Game/EnemyStateMachine.h
@@ -1,29 +1,51 @@
 #pragma once
 
 #include "FiniteStateMachine.h"
+#include "Enemy.h"
+
+enum TransitionType
+{
+	WANDER_TRANSITION = 0,
+	CHASE_TRANSITION = 1,
+	FLEE_TRANSITION = 2
+};
 
 struct Chase : public State
 {
-	State changeState(int choice)
-	{
-		if (choice == 2)
-		{
-			//Code for wander trigger
-		}
+	Chase(const SM_idType& id) :State(id) {};
 
-		else if (choice == 3)
-		{
-			//Code for Evade trigger
-		}
-	}
+	virtual void onEntrance(Enemy* me)
+	{
+		std::cout << "\nEntering Chase State: " << mID << std::endl;
+		//chase the player state
+
+	};
+	virtual void onExit(Enemy* me)
+	{
+		std::cout << "\nExiting Chase State: " << mID << std::endl;
+	};
+	virtual StateTransition* update()
+	{
+		std::cout << "Updating Chase" << std::endl;
+	};
 };
 
 struct Wander : public State
 {
+	Wander(const SM_idType& id) :State(id) {};
+
+	virtual void onEntrance();
+	virtual void onExit();
+	virtual StateTransition* update();
 };
 
-struct Evade : public State
+struct Flee : public State
 {
+	Flee(const SM_idType& id) :State(id) {};
+
+	virtual void onEntrance();
+	virtual void onExit();
+	virtual StateTransition* update();
 };
 
 class EnemyStateMachine : public FiniteStateMachine
--- GameAI/pathfinding/Pathfinding/Game/FiniteStateMachine.h
+++ GameAI/pathfinding/Pathfinding/Game/FiniteStateMachine.h
@@ -1,18 +1,59 @@
 #pragma once
 
 #include "Trackable.h"
+#include <map>
+
+class StateTransition;
+
+typedef int SM_idType;
 
 //Struct that serves as the base state class
 struct State
 {
-	State* changeState(int stateNum);
-private: 
-	int thisState;
+	State(const SM_idType& id) :mID(id) {};
+	~State();
+
+	void addTransition(StateTransition* pTransition);
+	inline const SM_idType& getID() const { return mID; };
+
+	virtual void onEntrance(Enemy* me) = 0;
+	virtual void onExit() = 0;
+
+	virtual StateTransition* update() = 0;
+protected: 
+	SM_idType mID;
+	std::map<TransitionType, StateTransition*> mTransitions;
 };
 
 class FiniteStateMachine : public Trackable
 {
-private:
-	State* currentState; //The current state of the state machine
+protected:
+	State* mpCurrentState; //The current state of the state machine
+	std::map<SM_idType, State*> mStates;
+	SM_idType mInitialStateID;
+
+	void transitionToState(const SM_idType& targetID);
 public:
+	FiniteStateMachine() {};
+	~FiniteStateMachine() {};
+
+	void addState(State* pState);
+	void setInitialStateID(const SM_idType& id) { mInitialStateID = id; };
+
+	void update();
+	void start();
 };
+
+class StateTransition : public Trackable
+{
+	StateTransition(TransitionType type, const SM_idType& targetID) :mType(type), mTargetStateID(targetID) {};
+	~StateTransition() {};
+
+	int getTargetStateID() const { return mTargetStateID; };
+	TransitionType getTransitionType() const { return mType; };
+
+protected:
+	SM_idType mTargetStateID;
+	TransitionType mType;
+};
+
